6d15b8f1c99bb974255873caba297b41df9e2176
WeberStack <157266813+WeberStack@users.noreply.github.com>
2024-02-07 23:26:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-require(["esri/Map", 
-         "esri/views/MapView", 
-         "esri/layers/FeatureLayer"], 
-        (Map, MapView, FeatureLayer) => {
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer","esri/core/urlUtils","esri/config"], (Map, MapView, FeatureLayer,urlUtils,esriConfig) => {
 //        var map = new Map({
 //          basemap: "hybrid"
 //        });
@@ -11,15 +8,17 @@ require(["esri/Map",
        //layers: [featureLayer_1]
       });
 
+          var template = {
+          title: "Earthquake Info",
+          content: "SChhol {Facility}"
+        };
+
      var view = new MapView({
        container:  "viewDiv",
        map: map,
        center: [ -90.1994, 38.6270 ],
        zoom: 10,
-          popup: {
-            actions: []
-          }
-      });
+          });
       
 
 var featureLayer_1 = new FeatureLayer({ url: "https://services2.arcgis.com/ZV8Mb62EedSw2aTU/ArcGIS/rest/services/CurrentSystem_REGISTERED_1/FeatureServer/4"
@@ -37,7 +36,8 @@ map.add(featureLayer_2);
   
 var featureLayer_3 = new FeatureLayer({
   url: 
-"https://services2.arcgis.com/kNS2ppBA4rwAQQZy/ArcGIS/rest/services/MO_Public_Schools/FeatureServer/0"
+"https://services2.arcgis.com/kNS2ppBA4rwAQQZy/ArcGIS/rest/services/MO_Public_Schools/FeatureServer/0",
+   popupTemplate: template
 });
 map.add(featureLayer_3);
 
